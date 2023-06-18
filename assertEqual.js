681f93caa6aa86cd53da936194da14f0504c33e6
lunamoonmoon <5862lunamoon@gmail.com>
2023-06-18 01:39:23 +0000

    initial commit

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+//function implementation
+const assertEqual = function(actual, expected) {
+  if(actual === expected) {
+<<<<<<< HEAD
+    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);    
+}
+};
+
+//test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("One", "One");
+assertEqual(1, 1.1);
+assertEqual(1, "One");

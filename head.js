beeacbddf88e0acc64ad8fbcca4f8fad961f7d44
lunamoonmoon <5862lunamoon@gmail.com>
2023-06-18 01:59:18 +0000

    finds first in array

---
+++ head.js
@@ -0,0 +1,18 @@
+//copy of assertEqual function
+const assertEqual = function(actual, expected) {
+  if(actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);    
+}
+};
+
+const head = function(array) {
+  let first = array[0];
+  console.log(first);
+  return(first);
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]), 5);

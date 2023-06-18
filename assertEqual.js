b9ce309505d38851c3b3ba19dd81d525763363e3
lunamoonmoon <5862lunamoon@gmail.com>
2023-06-18 01:17:40 +0000

    updated how string concatenates using template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 //function implementation
 const assertEqual = function(actual, expected) {
   if(actual === expected) {
-    console.log("🛑🛑🛑 Assertion Passed: " + actual + " === " + expected);
+    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("✅✅✅ Assertion Failed: " + actual + " !== " + expected);    
+    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);    
   }
 };
 

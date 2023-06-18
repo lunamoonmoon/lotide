8e89a0dd4ad9ed781b774d0b96bde0fc5424ec38
lunamoonmoon <5862lunamoon@gmail.com>
2023-06-18 02:23:45 +0000

    updates removed test code

--- assertEqual.js
+++ assertEqual.js
@@ -6,10 +6,3 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);    
 }
 };
-
-//test code
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("One", "One");
-assertEqual(1, 1.1);
-assertEqual(1, "One");
--- head.js
+++ head.js
@@ -13,6 +13,3 @@ const head = function(array) {
   return(first);
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);

931074756911025b93fafa95829493900e59db1b
lunamoonmoon <5862lunamoon@gmail.com>
2023-06-25 01:37:21 +0000

    find key function created with callback input

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if(actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);    
+}
+};
+
+function findKey(object, callback) {
+  for(let key in object) { //iterate through object key:values
+    if(callback(object[key])) {//if callback is true
+      return key;
+    }
+  }
+}
+
+
+let starGazer = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+console.log(assertEqual(starGazer, "noma"))
+

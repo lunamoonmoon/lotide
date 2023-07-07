7a54710bb9b9b6641c71532eab88ad687881c945
lunamoonmoon <5862lunamoon@gmail.com>
2023-07-07 21:10:04 +0000

    dryed up code using import and export, and seperated tests

--- tail.js
+++ tail.js
@@ -1,17 +1,9 @@
-//copy of assertEqual function
-const assertEqual = function(actual, expected) {
-  if(actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);    
-}
-};
 const tail = function(array) {
-  let rest = [];
-  for(let i = 1; i < array.length; i++) { //loops through array input starting at second value
+  let rest = []; //define new array
+  for (let i = 1; i < array.length; i++) { //loops through array input starting at second value
     rest.push(array[i]); //pushes to new array
   }
-  console.log(rest);
-  return(rest);
+  return rest;
 };
 
+module.exports = tail;

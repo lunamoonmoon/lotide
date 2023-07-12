2386429e60b78017283d8e579a5ba56bf08ec096
lunamoonmoon <5862lunamoon@gmail.com>
2023-07-12 02:59:28 +0000

    remove repeat code

--- countOnly.js
+++ countOnly.js
@@ -1,21 +1,3 @@
-function countOnly (allItems, itemsToCount) {
-  const result = {};
-  for (let myKey in itemsToCount) { //loop object keys
-    if (myKey) { //if object key is truthy
-      for (let i of allItems) { //loop through array
-        if (i === myKey) { //if value matches object key
-          if (!result[myKey]) {
-            result[myKey] = 0;
-          }
-          result[myKey] += 1; //add keyvalue to object and or increase by 1
-        }
-      }
-    }
-
-  }
-  return result;
-}
-
 function countOnly(allItems, itemsToCount) {
   const results = {};
   for (let item of allItems) { //loop array of items
